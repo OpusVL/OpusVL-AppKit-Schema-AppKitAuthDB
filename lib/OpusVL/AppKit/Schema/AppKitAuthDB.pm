abe0fa22c382138bc5520baf8fa7823fd9a4d12d
Colin Newell <colin@opusvl.com>
2016-06-28 13:37:57 +0100

    Initial commit

---
+++ lib/OpusVL/AppKit/Schema/AppKitAuthDB.pm
@@ -0,0 +1,26 @@
+use strict;
+use warnings;
+package OpusVL::AppKit::Schema::AppKitAuthDB;
+
+use strict;
+use warnings;
+our $VERSION = '1';
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+1;
+
+=head1 NAME
+
+OpusVL::AppKit::Schema::AppKitAuthDB - Auth Database for OpusVL::AppKit/FB11
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+
+=cut
